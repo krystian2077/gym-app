9039b165823a1e1960edcb9e9c0cb5a6e7fe32af
Krystian <krystian.potaczek07@gmail.com>
2023-04-30 23:59:02 +0200

    Init comit, configure tailwind and add navbar

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+
+const App = () => {
+  const [selectedPage, setSelectedPage] = useState<string>("home");
+  return (
+    <>
+      <div className="app bg-gray-20">
+        <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      </div>
+    </>
+  );
+};
+
+export default App;
